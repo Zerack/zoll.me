d71c2a753b15df74e9c104ee9eddf4d1eefa5e49
Jim Zoll <zolljd@gmail.com>
2013-04-03 09:09:10 -0400

    Updated .gitignore. Added settings.py
    Updated .gitignore to ignore local_settings.py, which contains settings
    of a personal / unique / sensitive nature. Added settings.py to the
    repository, which now contains only true Django configuration options.

---
+++ zoll_com/settings.py
@@ -0,0 +1,147 @@
+'''
+James D. Zoll
+
+1/20/2013
+
+Purpose: Defines django settings for the My Year In Temperatures project.
+         This module defines only settings not related directly to filesystem
+         paths or other sensitive information. That is, things like system paths,
+         database passwords, and secret key information are stored in local_settings.py
+         instead of here.
+
+License: This is a public work.
+
+'''
+
+# Library Imports
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
+# Local Imports
+from local_settings import *
+
+# DEBUG is imported from local_settings
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+          ('James Zoll', 'zolljd@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + ('django.core.context_processors.request','zoll_com.context_processors.installed_apps')
+
+LOGIN_URL = '/account/login'
+LOGOUT_URL = '/account/logout'
+
+ALLOWED_HOSTS = ['96.248.69.34','192.168.1.202']
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'America/New_York'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'zoll_com.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'zoll_com.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'scarf',
+    'rss',
+    'files',
+    'xbmc_photos',
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s - %(name)s: %(message)s'
+        }    
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
