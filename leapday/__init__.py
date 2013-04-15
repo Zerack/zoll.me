9981b38265edfbc578bd92db6c957129f83dac77
Jim Zoll <zolljd@gmail.com>
2013-04-14 22:01:49 -0400

    Updated __init__.py
    Fixed description, description_short. Added additional links for
    stupidtable and excanvas in uses.

--- leapday/__init__.py
+++ leapday/__init__.py
@@ -13,11 +13,14 @@ License: This is a public work.
 # linkbacks to the application, etc.
 app_info = {'name': 'Leap Day Recipedia',
             'icon': 'leapday/img/logo.png',
-            'description': 'A small gaming company called Spryfox has produced a fantastic little free to play game called Leap Day. A cooperative puzzle game, players must use surrounding resources to produce ever more complicated goods in the hopes of satisfying evil spirits.',
+            'description': 'A small gaming company called Spry Fox has produced a cooperative puzzle game called Leap Day. This project catalogs recipes that players must use to craft game-winning items. Recipes are searchable and sortable, and each has detailed production information.',
+            'description_short': 'This project catalogs recipes for the cooperative game Leap Day by Spry Fox. Recipes can be searched, and each has detailed production information.',
             'button_label': 'Leap On Over',
             'default_view': 'leapday.views.index',
             'display_priority': 85,
             'uses': [('postgreSQL','http://www.postgresql.org'),
                      ('Django','https://www.djangoproject.com'),
                      ('Bootstrap','http://twitter.github.com/bootstrap/'),
-                     ('jQuery','http://jquery.com/'),]}
+                     ('jQuery','http://jquery.com/'),
+                     ('Stupid-table-plugin', 'http://joequery.github.io/Stupid-Table-Plugin/'),
+                     ('ExplorerCanvas', 'http://excanvas.sourceforge.net/')]}
