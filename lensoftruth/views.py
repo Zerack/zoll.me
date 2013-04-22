8e71e0f1405ae4fc0cda1b4c93143bddc18505db
Jim Zoll <zolljd@gmail.com>
2013-04-22 11:54:39 -0400

    Base app structure in place.

--- lensoftruth/views.py
+++ lensoftruth/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+# Library Imports
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):    
+    return render_to_response('lensoftruth/index.html', context_instance = RequestContext(request))
