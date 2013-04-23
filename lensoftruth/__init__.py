0cc1d2836c119c8606321da951e4fd7768c4fa63
Jim Zoll <zolljd@gmail.com>
2013-04-23 10:22:50 -0400

    Update tests and init.

--- lensoftruth/__init__.py
+++ lensoftruth/__init__.py
@@ -13,7 +13,7 @@ License: This is a public work.
 # linkbacks to the application, etc.
 app_info = {'name': 'lensoftruth.js',
             'icon': 'lensoftruth/img/lensoftruth.png',
-            'description': 'jQuery has as very powerful data api to store strings and JavaScript objects on nodes in the DOM. These attributes are not directly visible in the DOM, so they can be hard to debug. The lensoftruth.js jQuery plugin fixes that by adding visible data attributes while debugging.',
+            'description': 'jQuery has as very powerful data API to store values and JavaScript objects on nodes in the DOM. These attributes are not directly visible in the DOM, so they can be hard to debug. The lensoftruth.js jQuery plugin fixes that by adding visible data attributes while debugging.',
             'description_short': 'This lightweight jQuery plugin adds visible data-* attributes directly in the DOM when you call $.data(), making debugging easy in all browsers.',
             'button_label': 'Reveal The Truth',
             'default_view': 'lensoftruth.views.index',
